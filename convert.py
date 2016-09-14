e75b59f3d7ba0619d96e64d089913015ec36e7a4
Ian Webster <ianw_github@ianww.com>
2016-09-14 08:21:28 -0700

    add parallax/distance so it's not just a big sphere

--- convert.py
+++ convert.py
@@ -5,21 +5,30 @@ import math
 
 def main():
     results = []
-    with gzip.open('data/TgasSource_000-000-000.csv.gz', 'rb') as csv_in:
-        reader = csv.DictReader(csv_in)
-        for row in reader:
-            ra = float(row['ra'])
-            dec = float(row['dec'])
-            # Conver to cartesian coords.
-            # http://www.stargazing.net/kepler/rectang.html
-            X = math.cos(ra) * math.cos(dec)
-            Y = math.sin(ra) * math.cos(dec)
-            Z = math.sin(dec)
-            name = row['tycho2_id'].strip()
-            if not name:
-                name = '?'
-            result = [name, X, Y, Z]
-            results.append(result)
+    paths = [
+        'data/TgasSource_000-000-000.csv.gz',
+        'data/TgasSource_000-000-001.csv.gz',
+    ]
+    for path in paths:
+        print 'Processing', path, '...'
+        with gzip.open(path, 'rb') as csv_in:
+            reader = csv.DictReader(csv_in)
+            for row in reader:
+                ra = float(row['ra'])
+                dec = float(row['dec'])
+                parallax = float(row['parallax'])
+                # Conver to cartesian coords.
+                # http://www.stargazing.net/kepler/rectang.html
+                X = math.cos(ra) * math.cos(dec) * parallax
+                Y = math.sin(ra) * math.cos(dec) * parallax
+                Z = math.sin(dec) * parallax
+                name = row['tycho2_id'].strip()
+                if not name:
+                    name = '?'
+                result = [name, X, Y, Z, parallax]
+                results.append(result)
+
+    print 'Writing...'
 
     with open('data/processed.js', 'w') as js_out:
         js_out.write('window.DATA=')
@@ -28,6 +37,8 @@ def main():
     with open('data/xyz.csv', 'w') as csv_out:
         for result in results:
             csv_out.write('%f,%f,%f\n' % (result[1], result[2], result[3]))
+
+    print 'Done.'
 
 if __name__ == '__main__':
     main()
