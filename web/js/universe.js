c7b7b1ebe6934946966ce344d7075c6ff63d4d6e
Ian Webster <ianw_github@ianww.com>
2016-09-14 07:30:42 -0700

    convert to latest threejs - it works

--- web/js/universe.js
+++ web/js/universe.js
@@ -14,21 +14,16 @@ function Universe(spaceviz, data) {
   };
 
   function setup(texture){
-    var particles = new THREE.Geometry();
-
-    attributes = {
-      size: {type: 'f', value: []},
-      r: {type: 'f', value: []},
-      g: {type: 'f', value: []},
-      b: {type: 'f', value: []},
-      pos: {type: 'v3', value: []}
-    };
-
     uniforms = {
       map: {type: 't', value: texture},
     };
 
-    for (var i=0; i < data.length; i++) {
+    var geometry = new THREE.BufferGeometry();
+    var positions = new Float32Array(data.length * 3);
+    var colors = new Float32Array(data.length * 3);
+    var sizes = new Float32Array(data.length);
+
+    for (var i=0, i3=0; i < data.length; i++, i3 += 3) {
       var datum = data[i];
       var name = datum[0];
       var pos = datum.slice(1, 4);
@@ -36,33 +31,32 @@ function Universe(spaceviz, data) {
       var x = pos[0] * SPREAD_FACTOR;
       var y = pos[1] * SPREAD_FACTOR;
       var z = pos[2] * SPREAD_FACTOR;
+      positions[i3 + 0] = x;
+      positions[i3 + 1] = y;
+      positions[i3 + 2] = z;
 
-      var point = new THREE.Vector3(x, y, z);
-      attributes.pos.value[i] = pos;
+      colors[i3 + 0] = 255;
+      colors[i3 + 1] = 255;
+      colors[i3 + 2] = 255;
 
-      var size = 2.0
-      attributes.size.value[i] = size;
-
-      attributes.r.value[i] = 100;
-      attributes.g.value[i] = 255;
-      attributes.b.value[i] = 100;
-
-      // add to mesh
-      particles.vertices.push(point);
+      sizes[i] = 2.0
     }
+
+    geometry.addAttribute( 'position', new THREE.BufferAttribute( positions, 3 ) );
+    geometry.addAttribute( 'color', new THREE.BufferAttribute( colors, 3 ) );
+    geometry.addAttribute( 'size', new THREE.BufferAttribute( sizes, 1 ) );
 
     var particle_material = new THREE.ShaderMaterial({
       uniforms: uniforms,
-      attributes: attributes,
       vertexShader: document.getElementById('vertex-shader').textContent,
-      fragmentShader: document.getElementById('fragment-shader').textContent
-    });
-    particle_material.depthTest = false;
-    particle_material.vertexColor = true;
-    particle_material.transparent = true;
-    particle_material.blending = THREE.AdditiveBlending;
+      fragmentShader: document.getElementById('fragment-shader').textContent,
 
-    var particle_system = new THREE.PointCloud(particles, particle_material);
+      depthTest: false,
+      transparent : true,
+      blending: THREE.AdditiveBlending
+    });
+
+    var particle_system = new THREE.Points(geometry, particle_material);
     particle_system.frustumCulled = true;
     particle_system.sortParticles = false;
     spaceviz.getScene().add(particle_system);
